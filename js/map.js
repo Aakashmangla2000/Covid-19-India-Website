a82c326d45bbf8f1dd477226a91f11e544285173
Aakash Mangla <akkibaby90@yahoo.com>
2020-06-02 00:22:02 +0530

    Added table with hover feature

--- js/map.js
+++ js/map.js
@@ -33,8 +33,8 @@ st = {
       "tg": "Telangana",
       "tn": "Tamil Nadu",
       "tr": "Tripura",
-      "tt": "1",
-      "un": "Unassigned",
+      "tt": "Total",
+      "un": "State Unassigned",
       "up": "Uttar Pradesh",
       "ut": "Uttarakhand",
       "wb": "West Bengal"
@@ -42,15 +42,17 @@ st = {
 
 console.log(Object.keys(st).length)
 
-async function getData(state){
+async function getData(){
     const result = await fetch(`https://api.covid19india.org/states_daily.json`)
     const data = await result.json();
     const data2 = data.states_daily;
+    return data2;
+}
 
+function changeValues(data2, state){
     let active = 0;
     let recovered = 0;
     let ded = 0;
-
     for (i = 0; i < data2.length; i++){
         if(data2[i].status === 'Confirmed'){
             active += parseInt(data2[i][state]);
@@ -69,18 +71,24 @@ async function getData(state){
    document.getElementById("recovered-cases").innerText = recovered;
    document.getElementById("ded-cases").innerText = ded;
    document.getElementById("state-name").innerText = st[state];
-
 }
 
 function check(){
     console.log('hi')
 }
 
-function hoverKara(s){
+async function hoverKara(s){
    // console.log(document.querySelector(`#${stateIds[1].id}`));
   //  var s = stateIds[eventLis.findIndex()].id.slice(3,5).toLowerCase()
-    getData(this.id.slice(3,5).toLowerCase())
- //  console.log(this.id)
+    data = await getData()
+    if(this.id.length>2)
+    changeValues(data,this.id.slice(3,5).toLowerCase())
+    else{    
+        changeValues(data,this.id)
+        document.getElementById(`IN-${this.id.toUpperCase()}`).style.fill = "red";
+       // document.getElementById(`IN-${this.id.toUpperCase()}`).style.fill = "rgb(243, 143, 132)";
+    }
+   // console.log(document.getElementById(`IN-${this.id.toUpperCase()}`))
 }
 
 stateIds = document.getElementsByTagName("path")
@@ -92,8 +100,55 @@ for(var i = 0; i<stateIds.length; i++){
  eventLis[i] = (document.getElementById(stateIds[i].id).addEventListener("mouseover", hoverKara))
 }
 
-aak = {
-    "as" : 1
+async function hoverHataya(s){
+   
+    document.getElementById(`IN-${this.id.toUpperCase()}`).style.fill = "rgb(243, 143, 132)";
+    console.log('hi')
+
 }
 
-a = 'as'
+
+
+
+
+
+async function createTable(){
+
+    console.log(st)
+    var data2;
+    state = st
+    data2 = await getData()
+    for (let [state2, value] of Object.entries(state)){
+        html = `<tr id="${state2}"><td>%st%</td><td>%c%</td><td>%a%</td><td>%r%</td><td>%d%</td></tr>`;
+        let active = 0;
+        let recovered = 0;
+        let ded = 0;
+        if(state2==='tt')
+        continue;
+        for (j = 0; j < data2.length; j++){
+        if(data2[j].status === 'Confirmed'){
+            active += parseInt(data2[j][state2]);
+        }
+        if(data2[j].status === 'Recovered'){
+            recovered += parseInt(data2[j][state2]);
+        }
+        if(data2[j].status === 'Deceased'){
+            ded += parseInt(data2[j][state2]);
+        }
+    }
+        nhtml = html.replace('%st%', st[state2]);
+        nhtml = nhtml.replace('%c%', active);
+        nhtml = nhtml.replace('%a%', active-recovered-ded);
+        nhtml = nhtml.replace('%r%', recovered);
+        nhtml = nhtml.replace('%d%', ded);
+
+
+        document.querySelector('#table-body').insertAdjacentHTML('beforeend',nhtml);
+        document.getElementById(state2).addEventListener("mouseover",hoverKara)
+        document.getElementById(state2).addEventListener("mouseleave",hoverHataya)
+
+    }
+}
+
+createTable();
+
